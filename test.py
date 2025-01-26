2c66f0837729c0d3f46df920efece63aad97689d
Pludam <109984063+Pludam@users.noreply.github.com>
2025-01-26 19:11:46 +0100

    Upload test.py

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 from parse.parsetimetableresponse import parse_timetable_response
 from json import JSONDecoder
 
+from dotenv import load_dotenv
+#load .env for user and password
+load_dotenv()
+
 with open("response.json","r") as f:
     print(parse_timetable_response(JSONDecoder().decode(f.read())))
