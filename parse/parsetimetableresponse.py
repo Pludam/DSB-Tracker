a17f1383e3d80e16eafe69f638f4b4f0ef043ce9
Pludam <109984063+Pludam@users.noreply.github.com>
2025-01-26 20:57:11 +0100

    Update Timetable Respose Parser to use dict

--- parse/parsetimetableresponse.py
+++ parse/parsetimetableresponse.py
@@ -1,8 +1,8 @@
 
 def parse_timetable_response(url_response:list)->dict:
-    timetables = []
+    timetables = {}
     
     for timetable in url_response:
-        timetables.append({timetable["Title"]: timetable["Childs"][0]["Detail"]})
-        print(timetables)
+        timetables[timetable["Title"]] = timetable["Childs"][0]["Detail"]
+
     return timetables
