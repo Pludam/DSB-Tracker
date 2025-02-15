9e8c460b4c256ac392b5939a1d61708304313d67
Pludam <109984063+Pludam@users.noreply.github.com>
2025-02-16 00:08:38 +0100

    implement lesson parsing

--- parse.py
+++ parse.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from util import format_line
 
+
 # timetable response parsing
 @format_line
 def parse_timetable_response(url_response: list) -> dict:
@@ -15,23 +16,49 @@ def parse_timetable_response(url_response: list) -> dict:
 def parse_timetable_html(html: str) -> dict:
     soup = BeautifulSoup(html, "lxml")
     curr_class = ""
-    lesson_hour = ""
+    curr_lesson_hour = ""
     teacher = ""
-    sub_type = ""
+    type = ""
+    lesson = ""
+    sub_lesson = ""
     room = ""
-    notes = ""
-    for tag in soup.find("table", {"class": "mon_list"}).children:
-        if tag  == "\n":
-            continue # skip tag elements that are whitespaces 
-        if tag.th: # skip elements with the tag th as a child because they contain labels
+    note = ""
+    timtable_data = []
+    if soup.find("table", {"class": "mon_list"}).tbody:
+        tags = soup.find("table", {"class": "mon_list"}).tbody.children  # needed because the html got from requests.get doesnt have tbody in the tables?
+    else:
+        tags = soup.find("table", {"class": "mon_list"}).children
+    for tag in tags:
+        if tag == "\n":
+            continue  # skip tag elements that are whitespaces
+        if (tag.th):  # skip elements with the tag th as a child because they contain labels
             continue
-        if tag.td.attrs["class"] == ["list","inline_header"]:
+        if tag.td.attrs["class"] == ["list", "inline_header"]:
             curr_class = tag.td.b.string
-            print(curr_class)
+            print("Class: " + curr_class)
             continue
-        for i,sub_tag in enumerate(tag.children): # class timetable structure: hour, teacher, type, lesson, room, possible notes
-            pass
-            
-
-        
-        
+        for i, sub_tag in enumerate(tag.children):  # class timetable structure: hour, teacher, type, lesson, room, possible note
+            match i:
+                case 0:
+                    curr_lesson_hour = sub_tag.b.string
+                    print("Current Hour: " + curr_lesson_hour)
+                case 1:
+                    pass
+                case 2:
+                    type = sub_tag.b.string
+                    print("Sub. Type: " + type)
+                case 3:
+                    if len(sub_tag.contents) == 2:
+                        lesson = sub_tag.contents[0].string
+                        sub_lesson = sub_tag.contents[1].replace("?", "")
+                        print(f"Lesson: {lesson}")
+                        print(f"Sub. Lesson: {sub_lesson}")
+                    else:
+                        lesson = sub_tag.string
+                        print(f"Lesson: {lesson}")
+                case 4:
+                    room = sub_tag.b.string
+                    print("Room: " + room)
+                case 5:
+                    pass
+        print("------------------------------")
--- test.py
+++ test.py
@@ -7,13 +7,20 @@ load_dotenv()
 
 timetableresponse = parse_timetable_response(fetch_timetable_data(get_auth_token()))
 
+local = True
 
 
 
 
+with open("timetable_response.html","w") as t:
+    t.write(requests.get(timetableresponse["Schülerplan Morgen - subst_001"]).text)
 
 
-parse_timetable_html(requests.get(timetableresponse["Schülerplan Heute - subst_001"]).text)
-""" with open("example_timetable.htm") as timetable:
-    #print(timetable.read())
-    parse_timetable_html(timetable.read()) """
+if local:
+#parse_timetable_html(requests.get(timetableresponse["Schülerplan Morgen - subst_001"]).text)
+    with open("example_timetable_browser.htm") as timetable:
+        
+        timetable_html = timetable.read()
+        parse_timetable_html(timetable_html)
+else:
+    parse_timetable_html(requests.get(timetableresponse["Schülerplan Morgen - subst_001"]).text)
